9a75e6394217cc025f5db35052cce70ddb52f391
Tim Caswell <tim@creationix.com>
2012-12-05 13:41:30 -0600

    Don't bother pretty-printing in the generator

--- brozula.js
+++ brozula.js
@@ -442,7 +442,7 @@ function compile(buffer, skipBuiltins) {
       if (i) line += ", ";
       line += slot(i);
     }
-    line += ") {";
+    line += "){";
     code.push(line);
 
     // Reserve a slow for the var line
@@ -456,34 +456,30 @@ function compile(buffer, skipBuiltins) {
     };
 
     if (needBlock) {
-      code.push("  var state = " + stateLabel(0) + ";");
-      code.push("  for(;;) {");
-      code.push("    loop: switch(state) {");
-      state.indent = "      ";
+      code.push("var state=" + stateLabel(0) + ";");
+      code.push("for(;;){");
+      code.push("switch(state){");
       targets[0] = true;
-    }
-    else {
-      state.indent = "  ";
     }
 
     // Compile the opcodes
     for (var i = 0, l = proto.bcins.length; i < l; i++) {
       var bc = proto.bcins[i];
       if (targets[i]) {
-        code.push("    case " + stateLabel(i) + ":");
+        code.push("case " + stateLabel(i) + ":");
       }
 
 //      console.error(bc);
-      var line = state.indent + generators[bc.op].apply(state, bc.args);
+      var line = generators[bc.op].apply(state, bc.args);
       if (conditionals[bc.op]) {
         var next = proto.bcins[++i];
-        line += " { " + generators[next.op].apply(state, next.args) + " }"
+        line += "{" + generators[next.op].apply(state, next.args) + "}"
       }
 //      console.error(line);
       code.push(line);
     }
     if (needBlock) {
-      code.push("    }\n  }");
+      code.push("}}");
     }
 
     var vars = [];
@@ -493,14 +489,14 @@ function compile(buffer, skipBuiltins) {
       vars.push(slot(i));
     }
     if (vars.length) {
-      code[varIndex] = " var " + vars.join(", ") + ";";
+      code[varIndex] = "var " + vars.join(",") + ";";
     }
     else {
       code[varIndex] = "";
     }
 
 
-    code.push("}\n");
+    code.push("}");
   });
 
   code.push("return " + pr(protos.length - 1) + ".apply(builtins, arguments);");
@@ -511,40 +507,40 @@ function compile(buffer, skipBuiltins) {
 
 var generators = {
   ISLT: function (a, d) {
-    return "if (" + slot(a) + " < " + slot(d) + ")";
+    return "if(" + slot(a) + "<" + slot(d) + ")";
   },
   ISGE: function (a, d) {
-    return "if (" + slot(a) + " > " + slot(d) + ")";
+    return "if(" + slot(a) + ">" + slot(d) + ")";
   },
   ISLE: function (a, d) {
-    return "if (" + slot(a) + " <= " + slot(d) + ")";
+    return "if(" + slot(a) + "<=" + slot(d) + ")";
   },
   ISGT: function (a, d) {
-    return "if (" + slot(a) + " >= " + slot(d) + ")";
+    return "if(" + slot(a) + ">=" + slot(d) + ")";
   },
   ISEQV: function (a, d) {
-    return "if (" + slot(a) + " === " + slot(d) + ")";
+    return "if(" + slot(a) + "===" + slot(d) + ")";
   },
   ISNEV: function (a, d) {
-    return "if (" + slot(a) + " !== " + slot(d) + ")";
+    return "if(" + slot(a) + "!==" + slot(d) + ")";
   },
   ISEQS: function (a, d) {
-    return "if (" + slot(a) + " === " + JSON.stringify(d) + ")";
+    return "if(" + slot(a) + "===" + JSON.stringify(d) + ")";
   },
   ISNES: function (a, d) {
-    return "if (" + slot(a) + " !== " + JSON.stringify(d) + ")";
+    return "if(" + slot(a) + "!==" + JSON.stringify(d) + ")";
   },
   ISEQN: function (a, d) {
-    return "if (" + slot(a) + " === " + d + ")";
+    return "if(" + slot(a) + "===" + d + ")";
   },
   ISNEN: function (a, d) {
-    return "if (" + slot(a) + " !== " + d + ")";
+    return "if(" + slot(a) + "!==" + d + ")";
   },
   ISEQP: function (a, d) {
-    return "if (" + slot(a) + " === " + JSON.stringify(d) + ")";
+    return "if(" + slot(a) + "===" + JSON.stringify(d) + ")";
   },
   ISNEP: function (a, d) {
-    return "if (" + slot(a) + " !== " + JSON.stringify(d) + ")";
+    return "if(" + slot(a) + "!==" + JSON.stringify(d) + ")";
   },
   ISTC: function (a, d) {
     throw new Error("TODO: Implement me");
@@ -553,98 +549,97 @@ var generators = {
     throw new Error("TODO: Implement me");
   },
   IST: function (d) {
-    return "if (!falsy(" + slot(d) + "))";
+    return "if(!falsy(" + slot(d) + "))";
   },
   ISF: function (d) {
-    return "if (falsy(" + slot(d) + "))";
+    return "if(falsy(" + slot(d) + "))";
   },
   MOV: function (a, d) {
-    return slot(a) + " = " + slot(d) + ";";
+    return slot(a) + "=" + slot(d) + ";";
   },
   NOT: function (a, d) {
-    return slot(a) + " = falsy(" + slot(d) + ");";
+    return slot(a) + "=falsy(" + slot(d) + ");";
   },
   UNM: function (a, d) {
-    return slot(a) + " = -" + slot(d) + ";";
+    return slot(a) + "=-" + slot(d) + ";";
   },
   LEN: function (a, d) {
-    return slot(a) + " = length(" + slot(d) + ");";
+    return slot(a) + "=length(" + slot(d) + ");";
   },
   ADDVN: function (a, b, c) {
-    return slot(a) + " = " + slot(b) + " + " + c + ";";
+    return slot(a) + "=" + slot(b) + "+" + c + ";";
   },
   SUBVN: function (a, b, c) {
-    return slot(a) + " = " + slot(b) + " - " + c + ";";
+    return slot(a) + "=" + slot(b) + "-" + c + ";";
   },
   MULVN: function (a, b, c) {
-    return slot(a) + " = " + slot(b) + " * " + c + ";";
+    return slot(a) + "=" + slot(b) + "*" + c + ";";
   },
   DIVVN: function (a, b, c) {
-    return slot(a) + " = " + slot(b) + " / " + c + ";";
+    return slot(a) + "=" + slot(b) + "/" + c + ";";
   },
   MODVN: function (a, b, c) {
-    return slot(a) + " = " + slot(b) + " % " + c + ";";
+    return slot(a) + "=" + slot(b) + "%" + c + ";";
   },
   ADDNV: function (a, b, c) {
-    return slot(a) + " = " + slot(c) + " + " + b + ";";
+    return slot(a) + "=" + slot(c) + "+" + b + ";";
   },
   SUBNV: function (a, b, c) {
-    return slot(a) + " = " + slot(c) + " - " + b + ";";
+    return slot(a) + "=" + slot(c) + "-" + b + ";";
   },
   MULNV: function (a, b, c) {
-    return slot(a) + " = " + slot(c) + " * " + b + ";";
+    return slot(a) + "=" + slot(c) + "*" + b + ";";
   },
   DIVNV: function (a, b, c) {
-    return slot(a) + " = " + slot(c) + " / " + b + ";";
+    return slot(a) + "=" + slot(c) + "/" + b + ";";
   },
   MODNV: function (a, b, c) {
-    return slot(a) + " = " + slot(c) + " % " + b + ";";
+    return slot(a) + "=" + slot(c) + "%" + b + ";";
   },
   ADDVV: function (a, b, c) {
-    return slot(a) + " = " + slot(b) + " + " + slot(c) + ";";
+    return slot(a) + "=" + slot(b) + "+" + slot(c) + ";";
   },
   SUBVV: function (a, b, c) {
-    return slot(a) + " = " + slot(b) + " - " + slot(c) + ";";
+    return slot(a) + "=" + slot(b) + "-" + slot(c) + ";";
   },
   MULVV: function (a, b, c) {
-    return slot(a) + " = " + slot(b) + " * " + slot(c) + ";";
+    return slot(a) + "=" + slot(b) + "*" + slot(c) + ";";
   },
   DIVVV: function (a, b, c) {
-    return slot(a) + " = " + slot(b) + " / " + slot(c) + ";";
+    return slot(a) + "=" + slot(b) + "/" + slot(c) + ";";
   },
   MODVV: function (a, b, c) {
-    return slot(a) + " = " + slot(b) + " % " + slot(c) + ";";
+    return slot(a) + "=" + slot(b) + "%" + slot(c) + ";";
   },
   POW: function (a, b, c) {
-    return slot(a) + " = Math.pow(" + slot(b) + ", " + slot(c) + ");";
+    return slot(a) + "=Math.pow(" + slot(b) + "," + slot(c) + ");";
   },
   CAT: function (a, b, c) {
-    var line = slot(a) + " = ''";
+    var line = slot(a) + "=''";
     for (var i = b; i <= c; i++) {
-      line += " + " + slot(i);
+      line += "+" + slot(i);
     }
     return line + ";";
   },
   KSTR: function (a, d) {
-    return slot(a) + " = " + JSON.stringify(d) + ";";
+    return slot(a) + "=" + JSON.stringify(d) + ";";
   },
   KCDATA: function (a, d) {
     throw new Error("TODO: Implement me");
   },
   KSHORT: function (a, d) {
-    return slot(a) + " = " + d + ";";
+    return slot(a) + "=" + d + ";";
   },
   KNUM: function (a, d) {
-    return slot(a) + " = " + d + ";";
+    return slot(a) + "=" + d + ";";
   },
   KPRI: function (a, d) {
-    return slot(a) + " = " + JSON.stringify(d) + ";";
+    return slot(a) + "=" + JSON.stringify(d) + ";";
   },
   KNIL: function (a, d) {
     var line = "";
     while (a <= d) {
-      line += slot(a) + " = null;";
-      if (a < d) line += "\n" + this.indent;
+      line += slot(a) + "=null;";
     }
     return line;
   },
@@ -667,53 +662,53 @@ var generators = {
     throw new Error("TODO: Implement me");
   },
   FNEW: function (a, d) {
-    return slot(a) + " = " + pr(d) + ".bind(this);";
+    return slot(a) + "=" + pr(d) + ".bind(this);";
   },
   TNEW: function (a, d) {
     var arrayn = d & 0x7ff;
     var hashn = d >>> 11;
-    if (!arrayn || hashn) return slot(a) + " = {};";
-    return slot(a) + " = new Array(" + arrayn + ");";
+    if (!arrayn || hashn) return slot(a) + "={};";
+    return slot(a) + "=new Array(" + arrayn + ");";
   },
   TDUP: function (a, d) {
     // TODO: handle more table types properly
-    return slot(a) + " = " + JSON.stringify(d) + ";";
+    return slot(a) + "=" + JSON.stringify(d) + ";";
   },
   GGET: function (a, d) {
     if (isIdent(d))
-      return slot(a) + " = this." + d + " || null;";
+      return slot(a) + "=this." + d + "||null;";
     else
-      return slot(a) + " = this[" + JSON.stringify(d) + "] || null;";
+      return slot(a) + "=this[" + JSON.stringify(d) + "]||null;";
   },
   GSET: function (a, d) {
     if (isIdent(d))
-      return "this." + d + " = " + slot(a) + ";";
+      return "this." + d + "=" + slot(a) + ";";
     else
-      return "this[" + JSON.stringfy(d) + "] = " + slot(a) + ";";
+      return "this[" + JSON.stringfy(d) + "]=" + slot(a) + ";";
   },
   TGETV: function (a, b, c) {
-    return slot(a) + " = " + slot(b) + "[" + slot(c) + "] || null;";
+    return slot(a) + "=" + slot(b) + "[" + slot(c) + "]||null;";
   },
   TGETS: function (a, b, c) {
     if (isIdent(c))
-      return slot(a) + " = " + slot(b) + "." + c + " || null;";
+      return slot(a) + "=" + slot(b) + "." + c + "||null;";
     else
-      return slot(a) + " = " + slot(b) + "[" + JSON.stringify(c) + "] || null;";
+      return slot(a) + "=" + slot(b) + "[" + JSON.stringify(c) + "]||null;";
   },
   TGETB: function (a, b, c) {
-    return slot(a) + " = " + slot(b) + "[" + c + "] || null;";
+    return slot(a) + "=" + slot(b) + "[" + c + "]||null;";
   },
   TSETV: function (a, b, c) {
-    return slot(b) + "[" + slot(c) + "] = " + slot(a) + ";";
+    return slot(b) + "[" + slot(c) + "]=" + slot(a) + ";";
   },
   TSETS: function (a, b, c) {
     if (isIdent(c))
-      return slot(b) + "." + c + " = " + slot(a) + ";";
+      return slot(b) + "." + c + "=" + slot(a) + ";";
     else
-      return slot(b) + "[" + JSON.stringify(c) + "] = " + slot(a) + ";";
+      return slot(b) + "[" + JSON.stringify(c) + "]=" + slot(a) + ";";
   },
   TSETB: function (a, b, c) {
-    return slot(b) + "[" + c + "] = " + slot(a) + ";";
+    return slot(b) + "[" + c + "]=" + slot(a) + ";";
   },
   TSETM: function () {
     throw new Error("TODO: Implement me");
@@ -721,24 +716,24 @@ var generators = {
   CALLM: function (a, b, c) {
     var line;
     if (b > 1) {
-      line = "$ = arr(";
+      line = "$=arr(";
       this.need$ = true;
     }
     else if (b == 0) {
-      line = "$$ = arr(";
+      line = "$$=arr(";
       this.need$$ = true;
     }
     else { line = ""; }
     if (c) {
-      line += slot(a) + ".apply(null, [";
+      line += slot(a) + ".apply(null,[";
       for (var i = a + 1; i <= a + c; i++) {
-        if (i > a + 1) line += ", ";
+        if (i > a + 1) line += ",";
         line += slot(i);
       }
       line += "].concat($$)";
     }
     else {
-      line += slot(a) + ".apply(null, $$";
+      line += slot(a) + ".apply(null,$$";
     }
     if (b === 1) {
       line += ");";
@@ -747,30 +742,30 @@ var generators = {
       line += "));";
     }
     if (b) {
-      line += "\n" + this.indent + "$$ = undefined;";
+      line += "$$=undefined;";
     }
     if (b > 1) {
       for (var i = 0; i < b - 1; i++) {
-        line += "\n" + this.indent + slot(a + i) + " = $[" + i + "];";
+        line += slot(a + i) + "=$[" + i + "];";
       }
-      line += "\n" + this.indent + "$ = undefined;";
+      line += "$=undefined;";
     }
     return line;
   },
   CALL: function (a, b, c) {
     var line;
     if (b > 1) {
-      line = "$ = arr(";
+      line = "$=arr(";
       this.need$ = true;
     }
     else if (b == 0) {
-      line = "$$ = arr(";
+      line = "$$=arr(";
       this.need$$ = true;
     }
     else { line = ""; }
     line += slot(a) + "(";
     for (var i = a + 1; i < a + c; i++) {
-      if (i > a + 1) line += ", ";
+      if (i > a + 1) line += ",";
       line += slot(i);
     }
     if (b === 1) {
@@ -781,9 +776,9 @@ var generators = {
     }
     if (b > 1) {
       for (var i = 0; i < b - 1; i++) {
-        line += "\n" + this.indent + slot(a + i) + " = $[" + i + "];";
+        line += slot(a + i) + "=$[" + i + "];";
       }
-      line += "\n" + this.indent + "$ = undefined;";
+      line += "$=undefined;";
     }
     return line;
   },
@@ -812,10 +807,10 @@ var generators = {
     throw new Error("TODO: Implement me");
   },
   RET0: function () {
-    return "return [];";
+    return "return[];";
   },
   RET1: function (a) {
-    return "return [" + slot(a) + "];"
+    return "return[" + slot(a) + "];"
   },
   FORI: function () {
     throw new Error("TODO: Implement me");
@@ -842,7 +837,7 @@ var generators = {
     throw new Error("TODO: Implement me");
   },
   LOOP: function () {
-    return "// LOOP";
+    return "";
   },
   ILOOP: function () {
     throw new Error("TODO: Implement me");
@@ -851,7 +846,7 @@ var generators = {
     throw new Error("TODO: Implement me");
   },
   JMP: function (a, d) {
-    return "state = " + stateLabel(d) + "; break loop;";
+    return "state=" + stateLabel(d) + ";break;";
   },
   FUNCF: function () {
     throw new Error("TODO: Implement me");
