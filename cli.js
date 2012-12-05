e918582e2bfa779c73cfb6cfb50a677c9ab4b053
Tim Caswell <tim@creationix.com>
2012-12-05 17:02:11 -0600

    Remove dead code from cli

--- cli.js
+++ cli.js
@@ -55,14 +55,6 @@ function compile(path, callback) {
   }
 }
 
-function uglify(orig_code) {
-  var jsp = require("uglify-js").parser;
-  var pro = require("uglify-js").uglify;
-  var ast = jsp.parse(orig_code); // parse code and get the initial AST
-  ast = pro.ast_mangle(ast); // get a new AST with mangled names
-  ast = pro.ast_squeeze(ast); // get an AST with compression optimizations
-  return pro.gen_code(ast); // compressed code here
-}
 var options = nopt({
   "execute": Boolean,
   "print": Boolean,
