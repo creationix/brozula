0117da301c24c82efeed984840ff3a16655c957c
Tim Caswell <tim@creationix.com>
2012-12-10 16:01:38 -0600

    Fix bugs in widgets example

--- widgets/dombuilder.lua
+++ widgets/dombuilder.lua
@@ -16,48 +16,55 @@ function domBuilder(json, refs)
 
   local jsonType = type(json)
   -- Render strings as text nodes
-  if jsonType == "string" then return document.createTextNode(json) end
+  if jsonType == "string" then
+    console.log("Found string, returning to text...", json)
+    return document.createTextNode(json)
+  end
 
   -- Pass through html elements and text nodes as-is
-  if isDom(json) then return json end
+  if isDom(json) then
+    console.log("Found dom, returning as-is...", json)
+    return json
+  end
 
   -- Stringify any other value types
   if not(jsonType == "table") then
+    console.log("Found unknown, returning stringified as text...", json)
     return document.createTextNode(tostring(json))
   end
 
   -- Empty arrays are just empty fragments.
   if #json == 0 then
+    console.log("found empty array, returning fragment...", json)
     return document.createDocumentFragment()
   end
 
-  local node, first, skip;
+  console.log("json", json)
+
+  local node, first;
   for i, part in ipairs(json) do
-    skip = false
 
     local partType = type(part)
-    if partType == "object" and next(part) == 1 then partType = "array" end
+    if partType == "table" and next(part) == 1 then partType = "array" end
 
-    if not node then
-      if partType == "string" then
-        -- Create a new dom node by parsing the tagline
-        local tag = part:match("^[^.$#]+") or "div"
-        node = document.createElement(tag)
-        first = true
-        local classes = {}
-        for class in part:gmatch("%.[^.$#]+") do
-          node.classList.add(class:sub(2))
-        end
-        local id = part:match("#[^.$#]+")
-        if id then node.setAttribute("id", id:sub(2)) end
-        local ref = part:match("%$[^.$#]+")
-        if ref then refs[ref:sub(2)] = node end
-        skip = true
-      else
+    if not node and partType == "string" then
+      -- Create a new dom node by parsing the tagline
+      local tag = part:match("^[^.$#]+") or "div"
+      node = document.createElement(tag)
+      first = true
+      local classes = {}
+      for class in part:gmatch("%.[^.$#]+") do
+        node.classList.add(class:sub(2))
+      end
+      local id = part:match("#[^.$#]+")
+      if id then node.setAttribute("id", id:sub(2)) end
+      local ref = part:match("%$[^.$#]+")
+      if ref then refs[ref:sub(2)] = node end
+      skip = true
+    else
+      if not node then
         node = document.createDocumentFragment()
       end
-    end
-    if not skip then
       -- Except the first item if it's an attribute object
       if first and partType == "table" then
         setAttrs(node, part)
