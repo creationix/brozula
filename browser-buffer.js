fe0f91f198ce9e5c9b08f95ffdca4b7aa4c72c14
Tim Caswell <tim@creationix.com>
2012-12-10 16:01:17 -0600

    Add a little more to node buffer clone

--- browser-buffer.js
+++ browser-buffer.js
@@ -1,6 +1,18 @@
 
 function Buffer(arraybuffer, offset, length) {
+  var data;
   if (arguments.length === 1) {
+    if (Array.isArray(arraybuffer)) {
+      data = arraybuffer;
+      arraybuffer = new ArrayBuffer(data.length);
+      var v = new Uint8Array(arraybuffer);
+      for (var i = 0, l = data.length; i < l; i++) {
+        v[i] = data[i];
+      }
+    }
+    else if (typeof arraybuffer === "number") {
+      arraybuffer = new ArrayBuffer(arraybuffer);
+    }
     offset = 0;
     length = arraybuffer.byteLength;
   }
@@ -14,15 +26,21 @@ function Buffer(arraybuffer, offset, length) {
 Buffer.prototype.__proto__ = Uint8Array.prototype;
 Buffer.prototype.slice = function (start, end) {
   return Buffer(this.buffer, start + this.offset, end - start);
-}
+};
 Buffer.prototype.readUInt32LE = function (offset) {
   return this.view.getUint32(offset, true);
 };
 Buffer.prototype.readUInt16LE = function (offset) {
   return this.view.getUint16(offset, true);
 };
-Buffer.prototype.readDoubleLE = function (value, offset) {
+Buffer.prototype.readInt16LE = function (offset) {
+  return this.view.getInt16(offset, true);
+};
+Buffer.prototype.readDoubleLE = function (offset) {
   return this.view.getFloat64(offset, true);
+};
+Buffer.prototype.writeDoubleLE = function (value, offset) {
+  return this.view.setFloat64(offset, value, true);
 };
 Buffer.prototype.writeUInt32LE = function (value, offset) {
   this.view.setUint32(offset, value, true);
@@ -31,4 +49,4 @@ Buffer.prototype.toString = function (encoding) {
   if (!encoding) {
     return String.fromCharCode.apply(null, this);
   }
-}
+};
